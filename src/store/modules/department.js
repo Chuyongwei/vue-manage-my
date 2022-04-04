0faa57f8c796ee98fa3dffdeb3449b4287f85f4e
Chuyongwei <1429303705@qq.com>
2022-04-04 22:23:48 +0800

    日历表

---
+++ src/store/modules/department.js
@@ -0,0 +1,94 @@
+import { logout, getInfo } from '@/api/user'
+import { removeToken } from '@/utils/auth'
+import { resetRouter } from '@/router'
+import { getDepartment } from '@/api/department'
+
+const getDefaultState = () => {
+  return {
+    departments: []
+  }
+}
+
+const state = getDefaultState()
+
+const mutations = {
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
+  },
+  SET_DEPARTMENT: (state, departments) => {
+    state.departments = departments
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_USER: (state, user) => {
+    state.user = user
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  },
+  SET_ROLES: (state, roles) => {
+    state.roles = roles
+  }
+}
+
+const actions = {
+  getDepartment({ commit }, store) {
+    getDepartment().then((data) => {
+      commit('SET_DEPARTMENT', data)
+    })
+  },
+
+  // get user info 这里的token就是医生的doctor——id号
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      getInfo(state.token).then(data => {
+        // const { data } = response
+        console.log('store获取的user', data)
+        if (!data) {
+          return reject('Verification failed, please Login again.')
+        }
+
+        const { name, roles } = data
+        commit('SET_USER', data)
+        commit('SET_NAME', name)
+        commit('SET_ROLES', roles)
+        // commit('SET_AVATAR', avatar)
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // user logout
+  logout({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      logout(state.token).then(() => {
+        removeToken() // must remove  token  first
+        resetRouter()
+        commit('RESET_STATE')
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      removeToken() // must remove  token  first
+      commit('RESET_STATE')
+      resolve()
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
---
+++ src/views/Score/Calendar.vue
@@ -0,0 +1,288 @@
+<template>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-input
+        v-model="listQuery.name"
+        placeholder="医生姓名"
+        style="width: 200px"
+        class="filter-item"
+        @keyup.enter.native="handleFilter"
+      />
+      <el-select
+        v-model="listQuery.departmentid"
+        placeholder="科室名称"
+        clearable
+        style="width: 120px"
+        class="filter-item"
+      >
+        <el-option
+          v-for="item in departments"
+          :key="item.departmentid"
+          :label="item.departmentname"
+          :value="item.departmentid"
+        />
+      </el-select>
+
+      <el-button
+        v-waves
+        class="filter-item"
+        type="primary"
+        icon="el-icon-search"
+        @click="handleFilter"
+      >
+        搜索
+      </el-button>
+      <!-- <el-button
+        class="filter-item"
+        style="margin-left: 10px"
+        type="primary"
+        icon="el-icon-edit"
+        @click="handleCreate"
+      >
+        添加医生
+      </el-button>
+      <el-button
+        v-waves
+        :loading="downloadLoading"
+        class="filter-item"
+        type="primary"
+        icon="el-icon-download"
+        @click="handleDownload"
+      >
+        导出图表
+      </el-button>
+      <el-checkbox
+        v-model="showReviewer"
+        class="filter-item"
+        style="margin-left: 15px"
+        @change="tableKey = tableKey + 1"
+      >
+        reviewer
+      </el-checkbox> -->
+    </div>
+    <div id="rota" style="width: 100%">
+      <!-- 第一行 -->
+      <div class="colum" style="font-family: '黑体'">
+        <div style="flex: 1" />
+        <div
+          v-for="{ value, index } of titles"
+          :key="index"
+          style="background: #458d71"
+          class="context"
+        >
+          {{ value }}
+        </div>
+      </div>
+      <!-- 第n行 -->
+
+      <!-- <div
+        v-for="(item,dinex) in dailys"
+        :key="dinex"
+        class="colum"
+        style="height: 90px"
+      >
+        <div class="firstcloumn" style="">
+          <font color="red" text-size="20em"> {{ item }}</font>
+        </div>
+        <div v-for="( context, index ) in titles" :key="index" class="context">
+          {{ dutyList[5].wid }}
+        </div>
+      </div> -->
+      <div
+        v-for="(item, row) in dutyList"
+        :key="row"
+        class="colum"
+        style="height: 90px"
+      >
+        <div class="firstcloumn" style="">
+          <font color="red" text-size="20em"> {{ dailys[row] }}</font>
+        </div>
+        <div
+          v-for="(context, col) in item"
+          :key="col"
+          class="context"
+          @click="updateDuty(context,row,col)"
+        >
+          {{ context.doctor_id == null ? '无' : context.name }}
+        </div>
+      </div>
+    </div>
+
+    <el-dialog title="更新" :visible.sync="dialogFormVisible">
+      <el-form
+        ref="dataForm"
+        :rules="rules"
+        :model="temp"
+        label-position="left"
+        label-width="70px"
+        style="width: 400px; margin-left: 50px"
+      >
+        <el-select
+          v-model="temp.doctor_id"
+          class="filter-item"
+          placeholder="Please select"
+        >
+          <el-option
+            v-for="item in doctors"
+            :key="item.doctorid"
+            :label="item.name"
+            :value="item.doctorid"
+          />
+        </el-select>
+        {{ temp.doctorid }}
+      </el-form>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false"> Cancel </el-button>
+        <el-button type="primary" @click="updateData()"> 提交 </el-button>
+      </div>
+    </el-dialog>
+
+    <!-- <el-table
+      style="width: 90%"
+      border
+      fit
+    >
+      <el-table-column label="" min-width="150px" align="center" />
+      <el-table-column label="星期一" width="150px" align="center" />
+      <el-table-column label="星期二" width="150px" align="center" />
+      <el-table-column label="星期三" width="150px" align="center" />
+      <el-table-column label="星期四" width="150px" align="center" />
+      <el-table-column label="星期五" width="150px" align="center" />
+      <el-table-column label="星期六" width="150px" align="center" />
+      <el-table-column label="星期六" width="150px" align="center" />
+    </el-table> -->
+  </div>
+</template>
+
+<script>
+import waves from '@/directive/waves' // waves directive
+export default {
+  name: 'Calendar',
+  directives: { waves },
+  data() {
+    return {
+      listQuery: {},
+      departments: [],
+      titles: [
+        { value: '星期一' },
+        { value: '星期二' },
+        { value: '星期三' },
+        { value: '星期四' },
+        { value: '星期五' },
+        { value: '星期六' },
+        { value: '星期七' }
+      ],
+      rules: {},
+      dialogFormVisible: false,
+      doctors: [],
+      dutyList: [],
+      tableData: [],
+      dailys: ['上午', '下午', '晚上'],
+      temp: {
+        doctorid: 0
+      },
+      qList: {
+        row: '',
+        col: ''
+      }
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      this.departments = this.$store.state.department.departments
+      this.$axios.get('/admin/checkDutyByDepartment').then((e) => {
+        const dutyList2 = e.data
+        for (let i = 0; i < 3; i++) {
+          this.dutyList.push(dutyList2.splice(0, 7))
+        }
+        console.log(this.dutyList)
+      })
+      const doctor = {
+        departmentid: 1
+      }
+      this.$axios.post('/doctor/check', doctor).then((e) => {
+        console.log(e)
+        this.doctors = e.data.items
+      })
+    },
+    handleFilter() {
+      console.log('搜索')
+    },
+    updateDuty(node, row, col) {
+      //   this.$axios.post('admin/updateDuty', node).then(e => {
+      //     console.log(e)
+      //   })
+      this.temp = {}
+      Object.assign(this.temp, node)
+      this.temp.doctorid = node.doctor_id
+      console.log(this.temp)
+      this.dialogFormVisible = true
+      this.qList.row = row
+      this.qList.col = col
+    },
+    updateData() {
+      // Object.assign(this.temp, { ...this.temp.doctor })
+      const doctor = this.doctors.filter((e) => {
+        return e.doctorid === this.temp.doctor_id
+      })
+      console.log(doctor)
+      this.temp.doctorid = this.temp.doctor_id
+      Object.assign(this.temp, { ...doctor[0] })
+      // this.temp.doctor = null
+      console.log(this.temp)
+      const row = this.qList.row
+      const col = this.qList.col
+
+      this.$axios.post('admin/updateDuty', this.temp).then(e => {
+        if (e.data > 0) {
+          const doctor = {
+            doctor_id: this.temp.doctorid,
+            name: this.temp.name
+          }
+          Object.assign(this.dutyList[row][col], doctor)
+          this.dutyList[row][col]
+          console.log(this.dutyList[row][col])
+        }
+      }).then(() => {
+        this.dialogFormVisible = false
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#rota {
+  margin: 30px;
+  font-weight: 500;
+  font-size: 1.3em;
+  font-family: "楷体";
+  text-align: center;
+  border: 1px #dfe6ec solid;
+}
+.firstcloumn {
+  padding-top: 1em;
+  border-radius: 3px;
+  border: 1px #dfe6ec solid;
+  flex: 1;
+  text-align: center;
+}
+.colum {
+  display: flex;
+  width: 90%;
+  background: #ffffff;
+  height: 80px;
+}
+.context {
+  padding-top: 1em;
+  border-radius: 3px;
+  border: 1px #dfe6ec solid;
+  width: 12%;
+  height: 100%;
+  background: #ffffff;
+}
+</style>
